f5d05e158f25bd41c6b1636d58dc8a2ef78309ae
Jhon Gonzalez <jhonja971106@gmail.com>
2022-02-22 21:19:57 -0500

    Creacion objeto de Usuarios para listar usuarios

--- src/app/pages/usuarios/usuarios.page.ts
+++ src/app/pages/usuarios/usuarios.page.ts
@@ -9,12 +9,12 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
   styleUrls: ['./usuarios.page.scss'],
 })
 export class UsuariosPage implements OnInit {
-  usuario: Usuario;
+
+  usuario: Usuario[] = [];
 
   constructor(private usuariosService: UsuariosService,
               private alertctrl: AlertController) { 
-    this.usuario = new Usuario();
-              }
+  }
 
   ngOnInit() {
     this.getUsuarios();
@@ -23,14 +23,17 @@ export class UsuariosPage implements OnInit {
   getUsuarios(){
     this.usuariosService.getUsuarios().subscribe(
       async response =>{
-        debugger
         if(response.success  === true){
-          this.usuario.idUsuario = response.usuarios.idUsuario;
-          this.usuario.contrasena = response.usuarios.contrasena;
-          this.usuario.usuario = response.usuarios.usuario;
-          this.usuario.rolUsuario = response.usuarios.rol;
-          this.usuario.cliente = response.usuarios.cliente
-  
+          response.data.usuarios.forEach((e,i)=>{
+            var nuevoU = new Usuario();
+            nuevoU.idUsuario = e.idUsuario;
+            nuevoU.usuario = e.usuario;
+            nuevoU.cliente = e.cliente;
+            nuevoU.rolUsuario = e.rol;
+            nuevoU.nomUsuario = e.nombre;
+            this.usuario.push(nuevoU);
+          })
+          // return this.usuario;
         }else{
           console.log(this.usuario);
           const alert = await this.alertctrl.create({
